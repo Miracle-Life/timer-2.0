6230889cfd92c058154ae22a2ae289954bd3ff6f
belov <belovdenis777@gmail.com>
2021-02-13 10:42:21 +0200

    timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {interval,Observable, timer} from "rxjs";
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-timer';
+  hour: string = '00';
+  minute: string = '00';
+  second: string = '00';
+  secondsStart: number = 0;
+  interval: any;
+  time: boolean = false;
+
+  constructor() {}
+  ngOnInit(): void {}
+
+  Time(){
+    let hour = Math.floor(this.secondsStart / (60 * 60));
+    let minute = Math.floor((this.secondsStart - hour * 60 * 60) / 60);
+    let second = this.secondsStart - hour * 60 * 60 - minute * 60;
+
+    let toZeroFirst = (x: number) => ('0' + x).slice(-2);
+
+    this.hour = toZeroFirst(hour);
+    this.minute = toZeroFirst(minute);
+    this.second = toZeroFirst(second);
+  }
+  switchStart() {
+    if (this.time) {
+      this.stop();
+    } else {
+      this.start();
+    }
+  }
+  start() {
+    this.interval = setInterval(() => {
+      this.secondsStart++;
+      this.Time();
+    }, 1000);
+    this.time = true;
+  }
+  reset() {
+    this.secondsStart = 0;
+    this.Time();
+  }
+  pause() {
+    clearInterval(this.interval);
+    this.time = false;
+    this.Time();
+  }
+  stop() {
+    clearInterval(this.interval);
+    this.time = false;
+    this.secondsStart = 0;
+    this.Time();
+  }
+
+
+
 }
